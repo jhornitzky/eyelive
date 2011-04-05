8545c06c71a1287a6d72df2ec34c04a8f1f1db70
James Hornitzky <james.hornitzky@gmail.com>
2011-04-05 12:42:36 +1000

    Updates to recorder to take picture instead of justvideo. More POC than working

--- NDroid/src/au/net/ravex/ndroid/NexRecorder.java
+++ NDroid/src/au/net/ravex/ndroid/NexRecorder.java
@@ -1,14 +1,23 @@
 package au.net.ravex.ndroid;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
 import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Environment;
 import android.util.Log;
 import android.view.Surface;
+import android.widget.ImageView;
 
 /**
  * Based on tutorial online
@@ -61,15 +70,17 @@ public class NexRecorder {
 		}
 
 		Log.d(TAG, "Starting recorder");
-
-		/* FIXME cameras
+		
  		Log.i(TAG, "Opening camera");
 		camera = Camera.open();
+		camera.takePicture(shutterCallback, rawCallback, jpegCallback); //try take a pciture by iteself
+		/*
+		//FIXME cameras
 		recorder.setCamera(camera);
 		recorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
 		recorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
 		*/
-
+		
 		//audio
 		recorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
 		recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
@@ -78,6 +89,42 @@ public class NexRecorder {
 		recorder.prepare();
 		recorder.start();
 	}
+	
+	// Called when shutter is opened
+	ShutterCallback shutterCallback = new ShutterCallback() { // <6>
+		public void onShutter() {
+			Log.d(TAG, "onShutter'd");
+		}		
+	};
+
+	// Handles data for raw picture
+	PictureCallback rawCallback = new PictureCallback() { // <7>
+		public void onPictureTaken(byte[] data, Camera camera) {
+			Log.d(TAG, "onPictureTaken - raw");
+		}
+	};
+	
+	// Handles data for jpeg picture
+	PictureCallback jpegCallback = new PictureCallback() { // <8>
+		public void onPictureTaken(byte[] data, Camera camera) {
+			FileOutputStream outStream = null;
+			try {
+				// Write to SD Card
+				outStream = new FileOutputStream(String.format(
+						"/sdcard/%d.jpg", System.currentTimeMillis())); // <9>
+				outStream.write(data);
+				outStream.close();
+				Log.d(TAG, "onPictureTaken - wrote bytes: " + data.length);
+			} catch (FileNotFoundException e) { // <10>
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				camera.release();
+			}
+			Log.d(TAG, "onPictureTaken - jpeg");
+		}
+	};
 
 	/**
 	 * Stops a recording that has been previously started.
